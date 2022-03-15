46228d8d0cdabfa17740c3b61f64c7966fcb00a4
draco-se <73908519+draco-se@users.noreply.github.com>
2022-03-15 22:40:48 +0100

    make it better

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
-   production: true,
-  restApiAddress: 'https://restapi-mobile-store.herokuapp.com',
-  frontEndAddress: 'https://angular-shop-project.herokuapp.com/auth',
-  publicKey: 'pk_test_51IwvJoJe2ZuBrbA8cGNV9yvaDcTgQUDGW8RJF5m1IlaHAHnnNUfX2GslRbysl0eCCrFg34bCYoSdpYklrJE9gf6R00a5Hxx4D6',
-  clientId: '889876418133-7v5u0dpe1bheeg3atfg8lvaf2lshj297.apps.googleusercontent.com'
+  production: true,
+  restApiAddress: 'http://localhost:5000',
+  frontEndAddress: 'http://localhost:4200/auth',
+  publicKey:
+    'pk_test_51IwvJoJe2ZuBrbA8cGNV9yvaDcTgQUDGW8RJF5m1IlaHAHnnNUfX2GslRbysl0eCCrFg34bCYoSdpYklrJE9gf6R00a5Hxx4D6',
+  clientId:
+    '889876418133-7v5u0dpe1bheeg3atfg8lvaf2lshj297.apps.googleusercontent.com',
 };
